9c46b729593af5ccfd1ef5d70d2088c3332b9b71
Abro0058T <genweb037@gmail.com>
2026-01-30 10:02:28 +0530

    Enhance Express template with new features and dependencies
    - Added new `app.ts` file to structure the Express application.
    - Introduced database connection management in `db.ts` with Mongoose.
    - Updated environment variable handling in `env.ts` to include database URL and node environment.
    - Enhanced `index.ts` to start the server and handle graceful shutdowns.
    - Updated `package.json` to include additional dependencies for TypeScript, Mongoose, and type definitions.

--- cli/src/templates/base-express/src/utils/env.ts
+++ cli/src/templates/base-express/src/utils/env.ts
@@ -3,10 +3,18 @@ import path from 'path'
 
 dotenv.config({path: path.resolve(__dirname, '../../.env')})
 
+const NODE_ENV = ['dev','prod','test'] as const
+
+type TNodeEnv = (typeof NODE_ENV)[number]
+
 export type TEnv = {
   port: number
+  db_url: string
+  node_env: TNodeEnv
 }
 
 export const env: TEnv = {
-  port: parseInt(process.env.PORT || '8000');
+  port: parseInt(process.env.PORT || '8000'),
+  db_url: process.env.MONGO_DB_URL || 'NA',
+  node_env:( process.env.NODE_ENV as TEnv['node_env']) || 'dev'
 }
